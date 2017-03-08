118a63f9308acd6e35af44cb03ab179cc5a0480e
GroverTB <grover.jtb@gmail.com>
2017-03-08 15:49:33 -0500

    [ADD] configure webpack and public path

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path    = require('path')
+const port    = process.env.PORT || 3000
+const app     = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('*', function (req, res){
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port)
+console.log("server started on port " + port)
