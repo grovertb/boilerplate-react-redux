f80738b6f59484988ae0a7845511b95b9527f3f5
GroverTB <grover.jtb@gmail.com>
2017-03-08 17:32:37 -0500

    [ADD] estructura

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-function component () {
-  var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello','webpack', 'sss'], ' ');
-  return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public', 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2016', 'es2017', 'react'],
+          compact: false,
+          plugins: ['transform-es2015-modules-commonjs']
+        }
+      }
+    ]
   }
 };
